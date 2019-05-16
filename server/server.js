dcbd60cd94b15628f5891c0c4011dfbaa4eca799
eugenia ong <eugenia.osy@gmail.com>
2019-05-16 11:16:02 -0700

    setup server skeleton

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('../db/data');
+
 const port = 2001;
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/photos', (req, res, next) => {
 
+});
 
-
-module.exports = {
-  port,
-};
+app.listen(port, console.log('listening on port', port));
